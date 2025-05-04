e45ca6289090329c073f488844a0ea6a101e0118
hakimrizm <hakimrizm05@gmail.com>
2025-05-04 21:27:16 +0700

    Menambahkan row baru dalam table article dan menambahkan factory, seeder kedalam table article

--- database/migrations/2025_05_04_130413_create_articles_table.php
+++ database/migrations/2025_05_04_130413_create_articles_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Validation\Rules\Unique;
 
 return new class extends Migration
 {
@@ -14,8 +15,8 @@ return new class extends Migration
         Schema::create('articles', function (Blueprint $table) {
             $table->id();
             $table->string('title');
+            $table->string('slug')->unique();
             $table->text('content');
-            $table->string('slug');
             $table->timestamps();
         });
     }
